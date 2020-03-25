7f1ac898e4cb93b4d46a3df711f8fc4782aafbc8
Adrian Faustino <contact.adrianfaustino@gmail.com>
2020-03-25 06:39:56 +0000

    Wrote function to iterate through a database and create new DOM elements

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,60 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(() => {
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      console.log('hi');
+      $('.tweets-container')
+        .append(createTweetElement(tweet));
+    }
+  };
+
+  const createTweetElement = function(obj) {
+    const newDiv = $('<div></div>')
+      .addClass('post-container');
+    
+    const newHeader = $('<header></header>')
+      .addClass('post')
+      .append(`<span class="username">${obj.user.name}</span>`)
+      .append(`<span class="userID">${obj.user.handle}</span>`);
+
+    const newPostContent = $(`<div>${obj.content.text}</div>`)
+      .addClass('post-content');
+
+    const newFooter = $(`<footer>${obj.created_at}</footer>`)
+      .addClass('tweet')
+
+    return newDiv
+      .append(newHeader)
+      .append(newPostContent)
+      .append(newFooter);
+  };
+
+  renderTweets(data);
+});
