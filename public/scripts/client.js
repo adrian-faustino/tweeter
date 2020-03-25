dc1ea0067ac8c961da590aefd0af58e36688d4f6
Adrian Faustino <contact.adrianfaustino@gmail.com>
2020-03-25 18:36:43 +0000

    Fixed GET and POST routes. Now able to dynamically add new posts after a user submits a new tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,35 +4,11 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
+// tweets parameter should be an array
 const renderTweets = function(tweets) {
   for (let tweet of tweets) {
     $('.tweets-container')
-      .append(createTweetElement(tweet));
+      .prepend(createTweetElement(tweet));
   }
 };
 
@@ -62,9 +38,26 @@ const createTweetElement = function(obj) {
     .append(newFooter);
 };
 
+const loadTweets = function(server) {
+  $.ajax(server, { method: 'GET' })
+    .then((data) => {
+      renderTweets(data);
+    });
+}
+
 // ==> After document loads
 $(document).ready(() => {
-  renderTweets(data);
+  loadTweets('/tweets');
 
-  const tweet_button = $();
+  $('#submission-form').on('submit', (e) => {
+    e.preventDefault();
+    
+    const tweetContent = $('#submission-form').serialize();
+
+    $.post('/tweets', tweetContent)
+      .then(() => {
+        loadTweets('/tweets');
+      });
+  });
 });
+
